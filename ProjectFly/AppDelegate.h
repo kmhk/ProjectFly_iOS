30f6ed3ad7b147492b2eb5566610461e6df2e0f0
hanjinghe <jinghe@han.local>
2015-02-20 23:15:23 +0800

    Initial Commit

---
+++ ProjectFly/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectFly
+//
+//  Created by han on 2/20/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
